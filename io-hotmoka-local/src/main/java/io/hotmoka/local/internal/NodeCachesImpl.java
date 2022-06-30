d756b74f3eddb98bf818f3dbc7d4d2e9087ab637
Fausto Spoto <fausto.spoto@gmail.com>
2022-06-30 14:36:25 +0200

    Moved to plain java.util.logging

--- io-hotmoka-local/src/main/java/io/hotmoka/local/internal/NodeCachesImpl.java
+++ io-hotmoka-local/src/main/java/io/hotmoka/local/internal/NodeCachesImpl.java
@@ -25,10 +25,9 @@ import java.util.Base64;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import io.hotmoka.beans.InternalFailureException;
 import io.hotmoka.beans.SignatureAlgorithm;
@@ -62,7 +61,7 @@ import io.hotmoka.nodes.ConsensusParams;
  * An implementation of the caches of a local node.
  */
 public class NodeCachesImpl implements NodeCaches {
-	protected final static Logger logger = LoggerFactory.getLogger(NodeCachesImpl.class);
+	protected final static Logger logger = Logger.getLogger(NodeCachesImpl.class.getName());
 
 	private final NodeInternal node;
 
@@ -310,7 +309,7 @@ public class NodeCachesImpl implements NodeCaches {
 				.build();
 		}
 		catch (Throwable t) {
-			logger.error("could not reconstruct the consensus parameters from the manifest", t);
+			logger.log(Level.SEVERE, "could not reconstruct the consensus parameters from the manifest", t);
 			throw InternalFailureException.of("could not reconstruct the consensus parameters from the manifest", t);
 		}
 	}
